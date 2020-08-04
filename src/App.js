7e91dbd00b72bcda249262bd0a63aed85718e100
Isaac Adorno <isaacadorno21@gmail.com>
2020-08-04 13:28:17 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import { Badge } from "shards-react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="example">
+      <Badge>Primary</Badge>
+      <Badge theme="secondary">Secondary</Badge>
+      <Badge theme="success">Success</Badge>
+      <Badge theme="info">Info</Badge>
+      <Badge theme="warning">Warning</Badge>
+      <Badge theme="danger">Danger</Badge>
+      <Badge theme="light">Light</Badge>
+      <Badge theme="dark">Dark</Badge>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <h1 style={{ textAlign: 'center' }}>Testing for AWS</h1>
+    <Grid container justify="center">
+      <Button variant="contained" color="secondary">
+        This button doesn't do anything
+      </Button>
+    </Grid>
+  </div>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
